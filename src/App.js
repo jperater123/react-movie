a112b80d2487150ffbb38a610fbe838c7930a148
jperater123 <jasperperater@gmail.com>
2024-08-21 13:26:42 +0800

    fix api

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SeachIcon from './search.svg';
 import MovieCard from "./MovieCard";
 
 //dd7b5838
-const API_URL = 'http://www.omdbapi.com?apikey=dd7b5838'
+const API_URL = 'https://www.omdbapi.com?apikey=dd7b5838'
 
 const movie1 = {
         "Title": "Amazing Spiderman Syndrome",
